ff010b8841092462b8720c3b1783cd460555121d
maximilionus <maximilionuss@gmail.com>
2022-09-12 23:19:59 +0300

    Улучшение патчера и README
    Патчер теперь может обнаруживать необходимые архивы находясь в корневой папке игры. Теперь нет необходимости помещать его в конкретную папку

--- README.md
+++ README.md
@@ -5,13 +5,15 @@
 <hr><br>
 
 # Основная информация
-- Данный мод меняет полностью все иконки кнопок в **NieR: Automata** c Xbox стиля на PS4 *(Dualshock 4)*
-- Теперь совместим с последними обновлениями игры и локализации.
-- Разработчики поленились добавить поддержку разных геймпадов, а [DualShock 4 Button Prompts](https://community.pcgamingwiki.com/files/file/870-nier-automata-dualshock-4-button-prompts/) за авторством Erewego совсем не дружит с [локализацией от ZOG](https://forum.zoneofgames.ru/topic/42805-nier-automata/). Вот и пришлось допиливать все своими ручками.
+- Данный патчер меняет полностью все иконки кнопок в **NieR: Automata** c Xbox стиля на PS4 *(Dualshock 4)*
+- Теперь совместим с последними обновлениями игры и локализации
+- Разработчики поленились добавить поддержку разных геймпадов, а [DualShock 4 Button Prompts](https://community.pcgamingwiki.com/files/file/870-nier-automata-dualshock-4-button-prompts/) за авторством Erewego совсем не дружит с [локализацией от ZOG](https://forum.zoneofgames.ru/topic/42805-nier-automata/). Вот и пришлось допиливать все своими ручками
 - Мод был проверен на лицензионной Steam версии. Если вы не имеете лицензионной копии игры, то гарантировать стабильную работу мода я вам не могу.
 - В репозитории есть все инструменты для ручного патчинга архивов в случае если что-то пойдет не так, все поломается, весь мир сгорит до тла и вам срочно понадобится исправить все самому. Я уверен - ты сможешь это сделать. Просто тебе придется прочитать немного документации ниже *(которую я еще не написал)*.
+- Оригинальный автор текстур PS4 Dualshock - [Spodi](https://www.nexusmods.com/nierautomata/mods/174)
 
 # Установка
-1. [Скачать архив с модом](https://github.com/maximilionus/NA_ZOG_PS4Prompts/releases/latest/download/NA_ZOG_PS4Prompts.zip) *(Самая актуальная версия)*
-2. Распаковать его содержимое в `<Директория_NieRAutomata>\data\ui`
-3. Ну и все, можно играть.
+1. [Скачать архив с патчером](https://github.com/maximilionus/NierA-ZOG-PS4-Prompts/releases/latest/download/PS4_Prompts_ZOG_Patcher.zip) *(Самая актуальная версия)*
+2. Распаковать файл `PS4 Prompts ZOG Patcher.exe` в корневую папку игры *(или же `<Директория_NieRAutomata>\data\ui`)*
+3. Запустить `PS4 Prompts ZOG Patcher.exe` и дождаться завершения работы
+4. Все готово, можно играть
--- src/patch.py
+++ src/patch.py
@@ -5,11 +5,7 @@ from subprocess import call, DEVNULL
 from shutil import copy, rmtree
 
 
-def show_intro():
-    print('NieR: Automata - RUS ZOG')
-    print('PS4 Prompts Patcher')
-    print('by @maximilionus <maximilionuss@gmail.com>')
-    print('\nДля правильной работы должен находиться в папке <NIER>\\data\\ui\n')
+__version__ = '2.0 Beta 1'
 
 
 def get_internal_dir() -> Path:
@@ -23,9 +19,69 @@ def get_internal_dir() -> Path:
 
 
 def clear_temp():
-    if temp_dir.exists():
-        rmtree(temp_dir)
+    if TEMP_DIR.exists():
+        rmtree(TEMP_DIR)
         notice_me('Временная директория удалена')
+
+
+EXECUTABLE_DIR = Path(sys.executable).parent
+GAME_DATA_UI_DIR = Path('data/ui/')
+
+if GAME_DATA_UI_DIR.exists():
+    # Detect if executable located in root of the game dir
+    # And switch to game directory with required files
+    EXECUTABLE_DIR = EXECUTABLE_DIR / GAME_DATA_UI_DIR
+
+INTERNAL_DIR = get_internal_dir()
+TEMP_DIR = EXECUTABLE_DIR / Path('.tmp/')
+PATH_BIN = INTERNAL_DIR / Path('bin/')
+
+PS4_PROMPTS_DIR = INTERNAL_DIR / Path('ps4_prompts/')
+
+QUICKBMS_DIR = PATH_BIN / Path('./quickbms/')
+QUICKBMS_EXEC = QUICKBMS_DIR / Path('quickbms.exe')
+QUICKBMS_SCRIPT_DTT = QUICKBMS_DIR / Path('scripts/dtt.bms')
+
+DTT_FILES = (
+    EXECUTABLE_DIR / Path('ui_core_us.dtt'),
+    EXECUTABLE_DIR / Path('ui_option_us.dtt')
+)
+
+
+def main():
+    # Ensure required .dtt files 're on place
+    notice_me('Проверяю, что все нужное на месте')
+    missing = [file for file in DTT_FILES if not file.exists()]
+    if len(missing) > 0:
+        print("Данные файлы не найдены в текущей дериктории:")
+        for missing_file in missing:
+            print('  ', missing_file.name)
+        return 0
+
+    # Extract .dtt
+    notice_me('Распаковка необходимых игровых архивов')
+    for file in DTT_FILES:
+        call_command([QUICKBMS_EXEC, '-Y', QUICKBMS_SCRIPT_DTT, file, TEMP_DIR / file.name[:-4]])
+
+    notice_me('Перемещаю PS4 иконки в распакованный архив')
+    copy(PS4_PROMPTS_DIR / 'ui_core_us_00000080.dds', TEMP_DIR / 'ui_core_us')
+    copy(PS4_PROMPTS_DIR / 'ui_option_us_00000080.dds', TEMP_DIR / 'ui_option_us')
+
+    # Inject the changes into the original .dtt archives
+    notice_me('Вшиваю измененные текстуры в оригинальные архивы')
+    for file in DTT_FILES:
+        call_command([QUICKBMS_EXEC, '-Y', '-w', '-r', QUICKBMS_SCRIPT_DTT, file, TEMP_DIR / file.name[:-4]])
+
+    notice_me('Готово! Очистка временных файлов...')
+    clear_temp()
+
+
+def show_intro():
+    print('NieR: Automata - RUS ZOG')
+    print('PS4 Prompts Patcher')
+    print(f'\nversion: {__version__}')
+    print('by @maximilionus <maximilionuss@gmail.com>')
+    print('\nДля правильной работы должен находиться в корневой папке игры или же в папке "<NIER>\\data\\ui"\n')
 
 
 def notice_me(text: str):
@@ -37,51 +93,6 @@ def notice_me(text: str):
 
 def call_command(args=[], output=DEVNULL):
     call(args, stdout=output, stderr=output)
-
-
-executable_dir = Path(sys.executable).parent
-internal_dir = get_internal_dir()
-temp_dir = executable_dir / Path('.tmp/')
-path_bin = internal_dir / Path('bin/')
-
-ps4_prompts_dir = internal_dir / Path('ps4_prompts/')
-
-quickbms_dir = path_bin / Path('./quickbms/')
-quickbms_exec = quickbms_dir / Path('quickbms.exe')
-quickbms_script_dtt = quickbms_dir / Path('scripts/dtt.bms')
-
-dtt_files = (
-    executable_dir / Path('ui_core_us.dtt'),
-    executable_dir / Path('ui_option_us.dtt')
-)
-
-
-def main():
-    # Ensure required .dtt files 're on place
-    notice_me('Проверяю, что все нужное на месте')
-    missing = [file for file in dtt_files if not file.exists()]
-    if len(missing) > 0:
-        print("Данные файлы не найдены в текущей дериктории:")
-        for missing_file in missing:
-            print('  ', missing_file.name)
-        return 0
-
-    # Extract .dtt
-    notice_me('Распаковка необходимых игровых архивов')
-    for file in dtt_files:
-        call_command([quickbms_exec, '-Y', quickbms_script_dtt, file, temp_dir / file.name[:-4]])
-
-    notice_me('Перемещаю PS4 иконки в распакованный архив')
-    copy(ps4_prompts_dir / 'ui_core_us_00000080.dds', temp_dir / 'ui_core_us')
-    copy(ps4_prompts_dir / 'ui_option_us_00000080.dds', temp_dir / 'ui_option_us')
-
-    # Inject the changes into the original .dtt archives
-    notice_me('Вшиваю измененные текстуры в оригинальные архивы')
-    for file in dtt_files:
-        call_command([quickbms_exec, '-Y', '-w', '-r', quickbms_script_dtt, file, temp_dir / file.name[:-4]])
-
-    notice_me('Готово! Очистка временных файлов...')
-    clear_temp()
 
 
 def pause():
